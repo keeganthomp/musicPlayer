524b202f4d47c84b2e08fbf71004866354d3c8c6
keeganthomp <whereiskeegan@gmail.com>
2017-06-15 19:20:23 -0500

    built out the logic for parsing data and formatting results

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 var button = document.querySelector(".searchBtn");
 var searchInput = document.querySelector(".searchBar");
+var submitBtn = document.querySelector(".searchBtn");
+var urlSc =
+  "http://api.soundcloud.com/users/52955/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f";
+var searchResultsContainer = document.querySelector(".searchResults");
 
-button.addEventListener("click", function() {
-  var searchInputResults = searchInput.value;
-  axios
-    .get(
-      "https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04"
-    )
-    .then(function(data) {
-      var data = data.data;
-      console.log(data);
-    });
-});
+const API_KEY = "?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f";
+
+function executeSearch(url) {
+  var urlSc =
+    "http://api.soundcloud.com/users/52955/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f";
+  axios.get(url).then(function(response) {
+    console.log(response.data);
+    for (let i = 0; i < 15; i++) {
+      createTracks(response.data[i]);
+    }
+  });
+}
+
+executeSearch(urlSc);
+
+function createTracks(data) {
+  function makeTrackWrapper() {
+    var createTrackWrapper = document.createElement("div");
+    createTrackWrapper.classList.add("trackWrapper");
+    searchResultsContainer.appendChild(createTrackWrapper);
+
+    var createArtistImage = document.createElement("img");
+    createArtistImage.classList.add("userImg");
+    createTrackWrapper.appendChild(createArtistImage);
+    createArtistImage.src = data.user.avatar_url;
+    
+    var createSongTitle = document.createElement("p");
+    createSongTitle.classList.add("songTitle");
+    createTrackWrapper.appendChild(createSongTitle);
+    createSongTitle.innerHTML = data.title;
+
+    var createUserName = document.createElement("p");
+    createUserName.classList.add("userName");
+    createTrackWrapper.appendChild(createUserName);
+    createUserName.innerHTML = data.user.username;
+    
+
+  }
+  makeTrackWrapper();
+}
